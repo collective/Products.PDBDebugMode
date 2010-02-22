9d0dbe4bb5ebe73433d0b564333ca24f60fdc8ac
Ross Patterson <me@rpatterson.net>
2010-02-22 22:58:20 +0000

    Don't check the matcher result when the matcher raised an exception

--- Products/PDBDebugMode/pdblogging.py
+++ Products/PDBDebugMode/pdblogging.py
@@ -29,8 +29,9 @@ def error(self, msg, *args, **kw):
         except:
             logger.exception('Matcher %r failed for log message %r' %
                              (matcher, msg))
-        if match:
-            break
+        else:
+            if match:
+                break
     else:
         if kw.get('exc_info'):
             type, value, traceback = sys.exc_info()
